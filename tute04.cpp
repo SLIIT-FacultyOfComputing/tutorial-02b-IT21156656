f72f8f77ad7083614bda9bfb8fd799c5f098d57e
IT21156656 <101384738+IT21156656@users.noreply.github.com>
2022-03-12 04:55:02 +0000

    completed

--- tute04.cpp
+++ tute04.cpp
@@ -13,17 +13,34 @@ Do not modify the main function.*/
 
 #include <iostream>
 
-long Factorial(int no);
-long nCr(int n, int r);
+long Factorial(int no); //function prototype
+long nCr(int a, int b);//function prototype
 
 int main() {
   int n, r;
-  std::cout << "Enter a value for n ";
+  std::cout << "Enter a value for n ";//get values from user
   std::cin >> n;
-  std::cout << "Enter a value for r ";
+  std::cout << "Enter a value for r ";//get values from user
   std::cin >> r;
   std::cout << "nCr = ";
   std::cout << nCr(n,r);
   std::cout << std::endl;
   return 0;
+}
+//find the factorial in function
+long Factorial(int no)
+{
+  int fac;
+  fac = 1;
+    for (int i=no; i >=1; i--)
+      {
+        fac = fac * i;
+      }
+  return fac;
+}
+long nCr(int a, int b)
+{
+  int cal;
+  cal=Factorial(a)/(Factorial(b)*Factorial(a-b));
+    return cal;
 }
